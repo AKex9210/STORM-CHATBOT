064a777b2a8f5607020df893712a8dad0f9d988d
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-25 14:57:28 +0800

    Add files via upload

---
+++ STORM/modules/ping.py
@@ -0,0 +1,38 @@
+
+
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import IMG, OWNER_USERNAME, STICKER
+from STORM import STORMX
+from STORM.database.chats import add_served_chat
+from STORM.database.users import add_served_user
+from STORM.modules.helpers import PNG_BTN
+
+
+@STORMX.on_cmd("ping")
+async def ping(_, message: Message):
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{VenomX.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [⏤͟͞〲Dᴇᴠɪʟ˹ᴀꜰᴋ˼🕊🥀](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
